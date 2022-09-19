51d471a8e560a67c12a67c3a705e6b23bf9437d0
Manon Brun <manonbrun0613@gmail.com>
2022-09-19 18:21:56 +0200

    Finish first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let i=0;
+    strRepeated='';
+    if (num==0) {
+        return ''
+    } else if (num<0){
+        return 'ERROR'
+    } else {
+        while (i<num) {
+            strRepeated=strRepeated +string;
+            i++;
+            }
+        return strRepeated
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString='';
+    let i=string.length-1;
+    console.log(reversedString, string,i);
+    let tempLetter = '';
+    let j=0;
+    while (j<=i) {
+        tempLetter=string.charAt(i-j);
+        reversedString=reversedString.concat(tempLetter)
+        j++;
+    }
+    return reversedString
 };
 
 // Do not edit below this line
