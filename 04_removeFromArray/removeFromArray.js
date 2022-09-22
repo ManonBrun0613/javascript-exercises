5f468620fc3066c7b2fc952bc8d45dac6966d4cd
Manon Brun <manonbrun0613@gmail.com>
2022-09-22 12:18:33 +0200

    Finish exercises four to seven

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,element1,element2,element3,element4) {
+    resultArray=[]
+    for (i of array) {
+        if (i ===element1  || i === element2|| i === element3|| i === element4) {
+            continue
+        } else {
+            resultArray = resultArray.concat(i)
+        }
+    }
+    return resultArray
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum=0;
+    let lowLimit=0;
+    let highLimit=0;
+    if (a>b) {
+        lowLimit=b;
+        highLimit=a;
+    } else {
+        lowLimit=a;
+        highLimit=b;
+    }
+    if (a <0 || b<0 || typeof(a)!= 'number' || typeof(b)!='number') {
+        return 'ERROR'
+    } else {
+        for (let i=lowLimit;i<=highLimit;i++) {
+            sum=sum+i;
+        }
+    }
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
